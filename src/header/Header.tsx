c391f216e3e58846d815b3a2e9f90e1a02789c19
cgrn24 <stlhnt42@gmail.com>
2023-01-24 03:25:17 +0700

    margin for mobile fix

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Nav } from '../nav/Nav'
-import style from './Header.module.css'
+import style from './Header.module.scss'
 export const Header = () => {
   return (
     <div className={style.header}>
