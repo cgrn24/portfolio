a9a9e790a3e67655dc77dc3b7d8117fd080542b9
cgrn24 <stlhnt42@gmail.com>
2023-01-27 03:59:04 +0700

    particles fix

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -18,20 +18,79 @@ export const Main = () => {
   const particlesLoaded = useCallback(async (container: Container | undefined) => {
     await console.log(container)
   }, [])
-  const particlesOptions = {
-    particles: {
-      number: {
-        value: 80,
-        density: {
-          enable: true,
-          value_area: 800,
-        },
-      },
-    },
-  }
+
   return (
     <div className={style.mainBlock}>
-      <Particles className={style.particles} params={particlesOptions} init={particlesInit} loaded={particlesLoaded} />
+      <Particles
+        className={style.particles}
+        options={{
+          fullScreen: { enable: false },
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: 'push',
+              },
+              onHover: {
+                enable: true,
+                mode: 'repulse',
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: '#ffffff',
+            },
+            links: {
+              color: '#ffffff',
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: 'none',
+              enable: true,
+              outModes: {
+                default: 'bounce',
+              },
+              random: false,
+              speed: 2,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 80,
+            },
+            shape: {
+              type: 'circle',
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+          detectRetina: true,
+        }}
+        init={particlesInit}
+        loaded={particlesLoaded}
+      />
       <div className={styleContainer.container}>
         <div className={style.text}>
           <span>Hi There</span>
