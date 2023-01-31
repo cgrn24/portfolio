e63b94fba602cb99dc7b6339b1ade31eb5fad4e3
cgrn24 <stlhnt42@gmail.com>
2023-01-31 22:47:51 +0700

    style for Link refactor

--- src/header/nav/Nav.tsx
+++ src/header/nav/Nav.tsx
@@ -3,16 +3,16 @@ import style from './Nav.module.scss'
 export const Nav = () => {
   return (
     <div className={style.nav}>
-      <Link activeClass={style.active} to='' spy={true} smooth={true} offset={0} duration={500}>
+      <Link className={style.link} activeClass={style.active} to='' spy={true} smooth={true} offset={0} duration={500}>
         Main
       </Link>
-      <Link activeClass={style.active} to='skills' spy={true} smooth={true} offset={1} duration={500}>
+      <Link className={style.link} activeClass={style.active} to='skills' spy={true} smooth={true} offset={1} duration={500}>
         Skills
       </Link>
-      <Link activeClass={style.active} to='projects' spy={true} smooth={true} offset={1} duration={500}>
+      <Link className={style.link} activeClass={style.active} to='projects' spy={true} smooth={true} offset={1} duration={500}>
         Projects
       </Link>
-      <Link activeClass={style.active} to='contacts' spy={true} smooth={true} offset={1} duration={500}>
+      <Link className={style.link} activeClass={style.active} to='contacts' spy={true} smooth={true} offset={1} duration={500}>
         Contacts
       </Link>
     </div>
