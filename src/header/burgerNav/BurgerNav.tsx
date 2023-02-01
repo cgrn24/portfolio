99d4cde53b0e5e1ab8869e5df7b1cc918c9e8bf7
cgrn24 <stlhnt42@gmail.com>
2023-02-02 04:31:47 +0700

    menu button added

--- src/header/burgerNav/BurgerNav.tsx
+++ src/header/burgerNav/BurgerNav.tsx
@@ -1,9 +1,14 @@
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useState } from 'react'
 import { Link } from 'react-scroll'
 import style from './BurgerNav.module.scss'
 export const BurgerNav = () => {
+  const [open, setOpen] = useState(false)
+  const clickHandler = () => setOpen(!open)
   return (
     <div className={style.burgerNav}>
-      <div className={style.burgerNavItems}>
+      <div className={open ? `${style.show} ${style.burgerNavItems}` : style.burgerNavItems}>
         <Link className={style.link} activeClass={style.active} to='' spy={true} smooth={true} offset={0} duration={500}>
           Main
         </Link>
@@ -17,6 +22,9 @@ export const BurgerNav = () => {
           Contacts
         </Link>
       </div>
+      {/* <div onClick={clickHandler} className={style.burgerBtn}> */}
+      <FontAwesomeIcon className={style.burgerBtn} onClick={clickHandler} icon={faBars} size='1x' color='#4e93e6' />
+      {/* </div> */}
     </div>
   )
 }
