c5232c4e343be496df85ad8692daf79cec15a8f3
cgrn24 <stlhnt42@gmail.com>
2023-02-05 04:52:20 +0700

    navigation offset fix

--- src/header/burgerNav/BurgerNav.tsx
+++ src/header/burgerNav/BurgerNav.tsx
@@ -12,10 +12,10 @@ export const BurgerNav = () => {
         <Link className={style.link} activeClass={style.active} to='main' spy={true} smooth={true} offset={0} duration={500}>
           Main
         </Link>
-        <Link className={style.link} activeClass={style.active} to='skills' spy={true} smooth={true} offset={1} duration={500}>
+        <Link className={style.link} activeClass={style.active} to='skills' spy={true} smooth={true} offset={-80} duration={500}>
           Skills
         </Link>
-        <Link className={style.link} activeClass={style.active} to='projects' spy={true} smooth={true} offset={1} duration={500}>
+        <Link className={style.link} activeClass={style.active} to='projects' spy={true} smooth={true} offset={-80} duration={500}>
           Projects
         </Link>
         <Link className={style.link} activeClass={style.active} to='contacts' spy={true} smooth={true} offset={1} duration={500}>
--- src/header/nav/Nav.tsx
+++ src/header/nav/Nav.tsx
@@ -6,10 +6,10 @@ export const Nav = () => {
       <Link className={style.link} activeClass={style.active} to='main' spy={true} smooth={true} offset={0} duration={500}>
         Main
       </Link>
-      <Link className={style.link} activeClass={style.active} to='skills' spy={true} smooth={true} offset={1} duration={500}>
+      <Link className={style.link} activeClass={style.active} to='skills' spy={true} smooth={true} offset={-80} duration={500}>
         Skills
       </Link>
-      <Link className={style.link} activeClass={style.active} to='projects' spy={true} smooth={true} offset={1} duration={500}>
+      <Link className={style.link} activeClass={style.active} to='projects' spy={true} smooth={true} offset={-80} duration={500}>
         Projects
       </Link>
       <Link className={style.link} activeClass={style.active} to='contacts' spy={true} smooth={true} offset={1} duration={500}>
